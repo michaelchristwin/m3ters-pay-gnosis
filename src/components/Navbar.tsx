95029327a6c9f9a5e991c3dd344e87b77573ba14
I. Christwin <iChristwin@gmail.com>
2025-02-25 21:52:03 +0100

    updating connect button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { ConnectKitButton } from "connectkit";
+import { Wallet } from "lucide-react";
 
 const Navbar = () => {
   return (
@@ -6,7 +7,17 @@ const Navbar = () => {
       className={`w-full h-[60px] flex justify-between items-center fixed top-0 left-0 bg-transparent px-4 py-3`}
     >
       <p></p>
-      <ConnectKitButton />
+      <ConnectKitButton.Custom>
+        {({ isConnected, show, truncatedAddress, ensName }) => {
+          return (
+            <button onClick={show} 
+              className="inline-flex items-center h-11 bg-blue-600 hover:bg-blue-700 transition-all duration-200"
+              >
+              {isConnected ? ensName ?? truncatedAddress : Connect <Wallet className="h-4 w-4" />}
+            </button>
+          );
+        }}
+      </ConnectKitButton.Custom>
     </nav>
   );
 };
